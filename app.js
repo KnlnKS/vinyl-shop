e4f0a05f14bf1c79ca74c40fbc63b8f2ab249e6c
KnlnKS <soulkks@hotmail.ca>
2020-10-12 15:49:35 -0400

    Spotify API Working
    Spotify API has now been successfully added to project.

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var app = require("express")(),
     clientId: "7f1c807b29964124956d0592ad6542df",
     clientSecret: "c4733ea98a184bcc9fdc69bc8e167b5b",
   }),
-  bodyParser = require("body-parser"),
-  SpotifyTrackList = require("./js/SpotifyTrackList");
+  bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -21,13 +20,15 @@ app.get("/", async (req, res) => {
 
   var credentials = await spotifyApi.clientCredentialsGrant();
   spotifyApi.setAccessToken(credentials.body["access_token"]);
+
   apiTrackList = (await spotifyApi.getPlaylist(id)).body.tracks.items;
 
   for (var i = 0; i < apiTrackList.length; i++) {
+
     trackList.push({
       releaseDate: apiTrackList[i].track.album.release_date,
       name: apiTrackList[i].track.name,
-      artistID: apiTrackList[i].track.artists[0].id,
+      albumArt: apiTrackList[i].track.album.images[0].url,
       artistName: apiTrackList[i].track.artists[0].name,
     });
   }
